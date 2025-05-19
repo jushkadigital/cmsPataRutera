a245a742b8563f5b05c42c26a29463926c72c30a
josh <urgosxd@gmail.com>
2025-05-19 15:04:54 -0500

    aoe

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,9 +1,9 @@
 import type { CollectionConfig } from 'payload'
-
 export const Users: CollectionConfig = {
   slug: 'users',
   admin: {
     useAsTitle: 'email',
+
   },
   auth: true,
   fields: [
