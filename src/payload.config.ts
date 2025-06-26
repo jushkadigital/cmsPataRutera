ae58c9bb2bc1106608d1c55f967e0400461f4c85
josh <urgosxd@gmail.com>
2025-06-25 21:37:26 -0500

    aoe

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -70,6 +70,28 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
     user: Users.slug,
+    livePreview: {
+      breakpoints: [
+        {
+          label: 'Mobile',
+          name: 'mobile',
+          width: 375,
+          height: 667,
+        },
+        {
+          label: 'Tablet',
+          name: 'tablet',
+          width: 768,
+          height: 1024,
+        },
+        {
+          label: 'Desktop',
+          name: 'desktop',
+          width: 1440,
+          height: 900,
+        },
+      ],
+    }
     // We'll add more components here later as needed
   },
   collections: [Users, Media, Pages, Tours, Ofertas, TourCategory, Destinations, BlogCategory, Post, Paquetes],
