af089f745c3570a8567d370e2a0e278260b4166e
josh <urgosxd@gmail.com>
2025-07-08 12:25:15 -0500

    aoe

--- src/collections/Tours/hooks/populatePrice.ts
+++ src/collections/Tours/hooks/populatePrice.ts
@@ -3,6 +3,9 @@ import type { CollectionAfterChangeHook, FieldHook } from 'payload'
 import type { Tour } from '@/payload-types'
 
 export const PopulatePrice: CollectionAfterChangeHook<Tour> = async ({ doc }) => {
+    if (!doc.layout) {
+        return doc
+    }
     if (doc.layout.length > 0) {
         doc.layout.map(block => {
             if (block.blockType == 'descrPrice') {
