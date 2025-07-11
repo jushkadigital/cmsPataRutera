6e5175320f6fb2f57e2a67a8509d1621582e436f
josh <urgosxd@gmail.com>
2025-07-11 17:27:33 -0500

    aoe

--- src/collections/Users/hooks/protectRoles.ts
+++ src/collections/Users/hooks/protectRoles.ts
@@ -3,12 +3,14 @@ import type { User } from '@/payload-types'
 
 export const protectRoles: FieldHook<{ id: string } & User> = ({ data, req }) => {
   const isAdmin = req.user?.roles?.includes('admin')
+  console.log(req.user?.roles)
+  console.log("ENTREE")
 
-  if (!isAdmin) {
-    return ['user']
-  }
+
 
   const userRoles = new Set(data?.roles || [])
+  console.log(userRoles)
   userRoles.add('user')
+  console.log(userRoles)
   return [...userRoles]
 }
